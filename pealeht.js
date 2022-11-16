705e0c69d083a590529b57c15d3f4828994c2ea0
marleneibrus <marlene.ibrus@gmail.com>
2022-11-16 22:11:00 +0200

    js, css ja kujundus

---
+++ pealeht.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function() {
+    const distance = window.scrollY
+    document.querySelector("header").style.transform = `translateY(${distance *
+      1}px)`
+    document.querySelector(
+      ".container"
+    ).style.transform = `translateY(${distance * 0.3}px)`
+    setTimeout(() => {
+      document.querySelector("section p").classList.add("animate-me")
+      document.querySelector("section h3").classList.add("animate-me")
+      document.querySelector("section img").classList.add("animate-me")
+    }, 400)
+  })
