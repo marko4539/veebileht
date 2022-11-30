fa16f7a3f54e6419e25cb07752c90921f6c13d53
marleneibrus <marlene.ibrus@gmail.com>
2022-11-30 18:30:10 +0200

    leht ja sisu

---
+++ digi/digi.js
@@ -0,0 +1,32 @@
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo_header');
+let logoSpan = document.querySelectorAll('.digidisain');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=> {
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1)*400)
+        });
+
+        setTimeout(()=> {
+            logoSpan.forEach((span, idx)=>{
+                
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1)*50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        },2300)
+
+
+    })
+})
