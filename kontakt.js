e71a4526339a4dbb2012b50db7faa462f0d3689d
marleneibrus <marlene.ibrus@gmail.com>
2022-12-05 20:48:14 +0200

    Viimased parandused ja JS kommenteerimine

--- kontakt.js
+++ kontakt.js
@@ -1,3 +1,6 @@
+// JavaScript pärineb kanali Beyond Fireship videost https://www.youtube.com/watch?v=T33NN_pPeNI
+
+// Loome konstandi observer, mis lisab igale elemendile, mis keritakse ekraanile, klassi ".show". Klassis ".show" on ära defineeritud animatsioon. Kui element keritakse ekraanilt välja, siis eemaldatakse klass ".show".
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -9,6 +12,7 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+// Loome konstandi "hiddenElements", milleks on kõik elemendid klassiga ".hidden" ja käsime prgrammil iga seda "hiddenElements" elementi konstandiga "observer" jälgida
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el));
 
